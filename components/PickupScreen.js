4b979fd7f9f6a7c1d7f1dc827216a1150a662df5
phanphuocthien <phanphuocthien@gmail.com>
2020-08-23 12:22:00 +0700

    add pickup screen

--- components/PickupScreen.js
+++ components/PickupScreen.js
@@ -1,18 +1,65 @@
 import * as React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, TouchableOpacity } from 'react-native';
 import { Component } from 'react';
 import firebase from 'firebase';
+import SquareGrid from "react-native-square-grid";
 
 export default class PickupScreen extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isModalVisible: false,
+            selectedIndex: -1,
+            selectedStatus: -1,
+            selectedTableNo: '',
+            modalTableNo: '',
+            isShowMessage: false,
+            message: '',
+            visible: false,
+            snapshot: null,
+            tables: [
+                [1,2,3],
+                [4,5],
+                [6,7,9,8],
+                []
+            ]
+        }
+    }
+
     render() {
+        let { tables} = this.state
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                
-            </View >
+            <SquareGrid rows={2} columns={2} items={tables} renderItem={this.renderItem} />
         );
     }
 
     resetTableData() {
         firebase.database().ref('/current').remove()
+    }
+
+    renderItem = (section, index) => {
+        return (
+            section.map(item=>{
+                return (
+                    <TouchableOpacity style={{
+                        flex: 1,
+                        alignSelf: 'flex-start',
+                        padding: 5
+                    }} onPress={() => {
+                        //this.showModal(item.status)
+                        //this.setState({ selectedIndex: index, selectedStatus: item.status, selectedTableNo: item.tableNo })
+                    }}>
+                        <View style={[{ borderRadius: 50 },
+                        { flex: 1 },
+                        {backgroundColor: "#42692f"},
+                        { alignItems: "center" },
+                        { justifyContent: "center" }
+                        ]}>
+                            <Text style={{ textAlign: 'center', color: '#fff', fontSize: 17, fontWeight: 'bold' }}>{item}</Text>
+                        </View>
+                    </TouchableOpacity>
+                )
+            })
+        );
     }
 }
